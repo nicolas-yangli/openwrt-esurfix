7355bc2316bc83cbacd1ce4538bf256959ea4922
Bill Lee <bill.lee.y@gmail.com>
2014-05-10 18:56:02 +0800

    revert travis ci configuration
    It seems impossible to finish the build in 50 min.
    
    Revert "fix travis ci configuration"
    
    This reverts commit 6a407d618617afa12dba1a2be677bcd1ba5f86c7.
    
    Revert "add travis ci configuration"
    
    This reverts commit 0e20c650e0ba7ce4cdb6dedf1ba2530ca9295f82.

--- .travis/build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-wget http://downloads.openwrt.org/attitude_adjustment/12.09/ramips/rt305x/config.ramips_rt305x -O .config \
-    && echo 'CONFIG_PACKAGE_chap-proxy=y' >> .config \
-    && make defconfig && make download && make
