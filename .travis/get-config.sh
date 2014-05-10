0e20c650e0ba7ce4cdb6dedf1ba2530ca9295f82
Bill Lee <bill.lee.y@gmail.com>
2014-05-10 17:29:34 +0800

    add travis ci configuration

---
+++ .travis/get-config.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+wget http://downloads.openwrt.org/attitude_adjustment/12.09/ramips/rt305x/config.ramips_rt305x -O .config \
+    && echo 'CONFIG_PACKAGE_chap-proxy=y' >> .config
---
+++ .travis/install-deps.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+apt-get update -qq
+apt-get install -y build-essential subversion git-core libncurses5-dev zlib1g-dev gawk flex quilt libssl-dev xsltproc libxml-parser-perl mercurial bzr ecj cvs unzip
