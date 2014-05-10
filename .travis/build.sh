6a407d618617afa12dba1a2be677bcd1ba5f86c7
Bill Lee <bill.lee.y@gmail.com>
2014-05-10 17:38:30 +0800

    fix travis ci configuration

--- .travis/build.sh
+++ .travis/build.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
-make defconfig && make download && make
+wget http://downloads.openwrt.org/attitude_adjustment/12.09/ramips/rt305x/config.ramips_rt305x -O .config \
+    && echo 'CONFIG_PACKAGE_chap-proxy=y' >> .config \
+    && make defconfig && make download && make
